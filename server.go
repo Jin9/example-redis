504b6fcb6199701fdf976d83cf80a2938412620a
Chinnawat <chinnawat@orcsoft.co.th>
2019-06-20 09:30:22 +0700

    refactor main

--- server.go
+++ server.go
@@ -1,42 +1,10 @@
 package main
 
 import (
-	// "wasabi/service"
-	// "wasabi/router"
-	"encoding/json"
-	"fmt"
-	"time"
-	"wasabi/db"
+	"wasabi/router"
 )
 
-// User is
-type User struct {
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-}
-
-// UnmarshalBinary is
-func (m *User) UnmarshalBinary(data []byte) error {
-	// convert data to yours, let's assume its json data
-	return json.Unmarshal(data, m)
-}
-
 func main() {
-	// e := router.Init()
-	// e.Logger.Fatal(e.Start(":8880"))
-
-	u := &User{
-		FirstName: "Adam",
-		LastName:  "Smith",
-	}
-
-	v, _ := json.Marshal(u)
-
-	db.SetData("key", v, 3*time.Second)
-	val := db.GetData("key")
-
-	data := &User{}
-	json.Unmarshal([]byte(val), &data)
-
-	fmt.Println(data)
+	e := router.Init()
+	e.Logger.Fatal(e.Start(":8880"))
 }
